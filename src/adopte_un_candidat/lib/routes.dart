bf8f0bd45dd733dc02cbac2584998f01bd9598d7
Mathias Gagnepain <mathias.gagnepain@algosup.com>
2024-06-04 12:12:34 +0200

    adding all required routes

--- src/adopte_un_candidat/lib/routes.dart
+++ src/adopte_un_candidat/lib/routes.dart
@@ -11,5 +11,47 @@ final GoRouter router = GoRouter(
         return const Home();
       },
     ),
+    GoRoute(
+      path: '/discussion',
+      name: 'discussion',
+      builder: (BuildContext context, GoRouterState state) {
+        return const Home(); // TODO: Replace with the discussion page
+      },
+    ),
+    GoRoute(
+      path: '/profile',
+      name: 'profile',
+      builder: (BuildContext context, GoRouterState state) {
+        return const Home(); // TODO: Replace with the profile page
+      },
+    ),
+    GoRoute(
+      path: '/settings',
+      name: 'settings',
+      builder: (BuildContext context, GoRouterState state) {
+        return const Home(); // TODO: Replace with the settings page
+      },
+    ),
+    GoRoute(
+      path: '/chat',
+      name: 'chat',
+      builder: (BuildContext context, GoRouterState state) {
+        return const Home(); // TODO: Replace with the chat page
+      },
+    ),
+    GoRoute(
+      path: '/login',
+      name: 'login',
+      builder: (BuildContext context, GoRouterState state) {
+        return const Home(); // TODO: Replace with the login page
+      },
+    ),
+    GoRoute(
+      path: '/register',
+      name: 'register',
+      builder: (BuildContext context, GoRouterState state) {
+        return const Home(); // TODO: Replace with the register page
+      },
+    )
   ],
 );
