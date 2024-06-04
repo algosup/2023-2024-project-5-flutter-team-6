b42238a8bb6b4c892e4f6f7b23e5e7af95295ca3
Mathias Gagnepain <mathias.gagnepain@algosup.com>
2024-06-04 16:09:05 +0200

    adding firebase authentication

--- src/adopte_un_candidat/lib/main.dart
+++ src/adopte_un_candidat/lib/main.dart
@@ -1,8 +1,12 @@
+import 'package:adopte_un_candidat/firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'routes.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+
   runApp(const MainApp());
 }
 
