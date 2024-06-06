91d59893a1017d9afe2ea21ce38828ae9db0fc3d
Mathias Gagnepain <mathias.gagnepain@algosup.com>
2024-06-06 14:22:43 +0200

    fix navbar issue and make it works <3

--- src/adopte_un_candidat/lib/widgets/navigation_bar.dart
+++ src/adopte_un_candidat/lib/widgets/navigation_bar.dart
@@ -1,3 +1,4 @@
+import 'package:adopte_un_candidat/modules/buttons.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:go_router/go_router.dart';
 import 'package:flutter/material.dart';
@@ -10,74 +11,60 @@ class SuperNavigationBar extends StatefulWidget {
 }
 
 class _SuperNavigationBarState extends State<SuperNavigationBar> {
+  int _selectedIndex = 0;
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      bottomNavigationBar: BottomAppBar(
-        child: Padding(
-          padding: const EdgeInsets.fromLTRB(25.0, 0.0, 25.0, 50.0), // Espace sur les côtés et en bas
-          child: Container(
-            decoration: BoxDecoration(
-              border: Border.all(
-                width: 2,
-                color: Colors.blue, // Remplace `myGradientMenu.colors.first` par une couleur de ton choix
-              ),
-              borderRadius: BorderRadius.circular(100),
+    return BottomNavigationBar(
+        items: <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(
+              CupertinoIcons.chat_bubble_2,
+              size: MediaQuery.of(context).size.width > 600
+                  ? MediaQuery.of(context).size.width * 0.08
+                  : MediaQuery.of(context).size.width * 0.10,
             ),
-            child: Center(
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  IconButton(
-                    onPressed: () {
-                      context.pushNamed('messages');
-                    },
-                    icon: Icon(
-                      CupertinoIcons.chat_bubble_2,
-                      size: MediaQuery.of(context).size.width > 600
-                          ? MediaQuery.of(context).size.width * 0.08
-                          : MediaQuery.of(context).size.width * 0.10,
-                    ),
-                  ),
-                  Container(
-                    decoration: const BoxDecoration(
-                      shape: BoxShape.circle,
-                      gradient: LinearGradient(
-                        colors: [Colors.blue, Colors.purple], // Remplace par `myGradientMenu` si nécessaire
-                      ),
-                    ),
-                    child: IconButton(
-                      padding: const EdgeInsets.all(10),
-                      onPressed: () {
-                        context.pushNamed('home');
-                      },
-                      icon: Icon(
-                        CupertinoIcons.rectangle_stack,
-                        size: MediaQuery.of(context).size.width > 600
-                            ? MediaQuery.of(context).size.width * 0.08
-                            : MediaQuery.of(context).size.width * 0.10,
-                        color: Colors.white,
-                      ),
-                    ),
-                  ),
-                  IconButton(
-                    onPressed: () {
-                      context.pushNamed('profile');
-                    },
-                    icon: Icon(
-                      CupertinoIcons.person_fill,
-                      size: MediaQuery.of(context).size.width > 600
-                          ? MediaQuery.of(context).size.width * 0.08
-                          : MediaQuery.of(context).size.width * 0.10,
-                    ),
-                  ),
-                ],
-              ),
-            ),
+            label: 'messages',
           ),
-        ),
-      ),
-    );
+          BottomNavigationBarItem(
+            icon: Icon(
+              CupertinoIcons.rectangle_stack,
+              size: MediaQuery.of(context).size.width > 600
+                  ? MediaQuery.of(context).size.width * 0.08
+                  : MediaQuery.of(context).size.width * 0.10,
+            ),
+            label: 'home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(
+              CupertinoIcons.person_fill,
+              size: MediaQuery.of(context).size.width > 600
+                  ? MediaQuery.of(context).size.width * 0.08
+                  : MediaQuery.of(context).size.width * 0.10,
+            ),
+            label: 'profile',
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        selectedItemColor: myGradientMenu.colors.first,
+        showSelectedLabels: false, // Hide labels for selected items
+        showUnselectedLabels: false, // Hide labels for unselected items
+        onTap: (int index) {
+          switch (index) {
+            case 0:
+              context.pushNamed('discussion');
+            case 1:
+              context.pushNamed('home');
+            case 3:
+              context.pushNamed('profile');
+          }
+          setState(
+            () {
+              _selectedIndex = index;
+            },
+          );
+        },
+      );
   }
 }
 
