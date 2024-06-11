65f25efb38695970931b5c21a77e2ed03f268238
Despaux <guillaume.despaux@algosup.com>
2024-06-11 19:25:16 +0200

    db with softskill start

--- src/adopte_un_candidat/lib/softskills.dart
+++ src/adopte_un_candidat/lib/softskills.dart
@@ -1,6 +1,8 @@
 import 'package:adopte_un_candidat/widgets/navigation_bar.dart';
 import 'package:flutter/material.dart';
 import './widgets/soft_skills_button.dart';
+import 'package:carousel_slider/carousel_slider.dart';
+import './modules/database.dart';
 
 class Softskills extends StatefulWidget {
   const Softskills({super.key});
@@ -10,19 +12,69 @@ class Softskills extends StatefulWidget {
 }
 
 class _SoftskillsState extends State<Softskills> {
+  Database database = Database();
+  var categories;
+  List<Widget> carouselItems = [];  // Assure que la liste contient des widgets
+
+  @override
+  void initState() {
+    super.initState();
+    database.softSkills().then((value) {
+      setState(() {
+        categories = value;
+
+          for(List category in categories) {
+          print(category);
+
+          carouselItems.add(
+            Container(
+              margin: EdgeInsets.symmetric(horizontal: 5.0),
+              decoration: BoxDecoration(
+                color: Colors.amber,
+                borderRadius: BorderRadius.circular(10.0),
+              ),
+              child: Center(
+                child: Text(
+                  category.toString(),
+                  style: TextStyle(fontSize: 16.0, color: Colors.white),
+                ),
+              ),
+            ),
+          );
+        }
+      });
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
       appBar: AppBar(
+        backgroundColor: Colors.white,
         title: const Text('Soft Skills'),
       ),
-      body: const Column(
-        children: [
-          SoftSkillsButton(type: 'Social',content: 'Insulte',),
-          SoftSkillsButton(type: 'Analytical',content: 'Super Mega Supra Long Texte De La Mort Qui Tue',),
-          SoftSkillsButton(type: 'Social',content: 'Insulte',),
-        ],
+      body: Container(
+        height: MediaQuery.of(context).size.height,
+        margin: const EdgeInsets.all(32),
+        decoration: BoxDecoration(
+          shape: BoxShape.rectangle,
+          borderRadius: BorderRadius.circular(30),
+          color: const Color(0xFFE8E8E8),
+        ),
+        child: categories
+          ? Center(child: CircularProgressIndicator())
+          : ListView(
+              children: [
+                CarouselSlider(
+                  items: carouselItems,
+                  options: CarouselOptions(
+                    autoPlay: false,
+                    height: 100,
+                  ),
+                ),
+              ],
+            ),
       ),
       bottomNavigationBar: const SuperNavigationBar(
         index: 2,
--- src/adopte_un_candidat/lib/widgets/soft_skills_button.dart
+++ src/adopte_un_candidat/lib/widgets/soft_skills_button.dart
@@ -1,13 +1,13 @@
 import 'dart:ffi';
-
 import 'package:flutter/material.dart';
+import '../modules/database.dart';
 
 class SoftSkillsButton extends StatefulWidget {
-  final String type;
-  final String content;
+  final String titles;
+  final String skills;
 
   const SoftSkillsButton(
-      {super.key, required this.content, required this.type});
+      {super.key, required this.skills, required this.titles});
 
   @override
   State<SoftSkillsButton> createState() => _SoftSkillsButtonState();
@@ -21,20 +21,20 @@ class _SoftSkillsButtonState extends State<SoftSkillsButton> {
     Color background;
     Color textColor;
 
-    switch (widget.type) {
-      case 'Social':
+    switch (widget.titles) {
+      case 'social':
         background = const Color(0xFF38A0FF);
         textColor = const Color(0xFFFFFFFF);
         break;
-      case 'Self-management':
+      case 'self-management':
         background = const Color(0xFFFF7474);
         textColor = const Color(0xFFFFFFFF);
         break;
-      case 'Analytical':
+      case 'analytical':
         background = const Color(0xFFFFFC4F);
         textColor = const Color(0xFF000000);
         break;
-      case 'Interpersonal':
+      case 'interpersonal':
         background = const Color(0xFF84FF7B);
         textColor = const Color(0xFF000000);
         break;
@@ -62,11 +62,29 @@ class _SoftSkillsButtonState extends State<SoftSkillsButton> {
           padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 4),
           margin: const EdgeInsets.symmetric(horizontal: 8, vertical: 4),
           child: Text(
-            widget.content,
+            widget.skills,
             style: TextStyle(color: textColor),
           ),
         ),
       ),
     );
+  }
+}
+
+class SoftSkillsList extends StatelessWidget {
+  final List<dynamic> categories;
+
+  SoftSkillsList({super.key, required this.categories});
+  // List skills = categories.skills.en;
+  // String titles = categories.titles.en;
+  int i = 0;
+  
+  @override
+  Widget build(BuildContext context) {
+    // for(i < skills.lenght()){
+    //   String skill = skills[i];
+    //   categories.add(SoftSkillsButton(skills: skill, titles: titles),);
+    // }
+    return Container();
   }
 }
