ba9dc1a4890542056e6c250ee6df2e92b6ade141
Mathias Gagnepain <mathias.gagnepain@algosup.com>
2024-06-13 14:24:13 +0200

    adding scroll to message list

--- src/adopte_un_candidat/lib/chat.dart
+++ src/adopte_un_candidat/lib/chat.dart
@@ -4,6 +4,7 @@ import 'package:adopte_un_candidat/widgets/navigation_bar.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/scheduler.dart';
 
 class Chat extends StatefulWidget {
   final String conversationId;
@@ -15,6 +16,7 @@ class Chat extends StatefulWidget {
 
 class ChatState extends State<Chat> {
   final TextEditingController messageController = TextEditingController();
+  late ScrollController scrollController;
   dynamic user;
   dynamic secondaryUser;
   dynamic messages;
@@ -24,12 +26,12 @@ class ChatState extends State<Chat> {
   void initState() {
     super.initState();
     conversationId = widget.conversationId;
-    Authentication().getCurrentUser().then(
-      (value) {
-        setState(() {
-          user = value;
-        });
-        Database().getMessages(value!.uid).then(
+    scrollController = ScrollController();
+    Authentication().getCurrentUser().then((value) {
+      setState(() {
+        user = value;
+      });
+      Database().getMessages(value!.uid).then(
         (data) {
           setState(() {
             messages = data![conversationId]["messages"];
@@ -38,13 +40,33 @@ class ChatState extends State<Chat> {
           Database().readMessage(conversationId, user.uid);
         },
       );
-      }
-    );
+    });
   }
 
-  
+  @override
+  void dispose() {
+    scrollController.dispose();
+    super.dispose();
+  }
 
-  Widget senderMessage(String content, String picture){
+  void scrollToBottom() {
+    SchedulerBinding.instance!.addPostFrameCallback((_) {
+      if (scrollController.hasClients) {
+        scrollController.animateTo(
+          scrollController.position.maxScrollExtent,
+          duration: const Duration(milliseconds: 300),
+          curve: Curves.easeOut,
+        );
+      } else {
+        if (kDebugMode) {
+          print("ScrollController has no clients");
+        }
+      }
+    });
+  }
+
+  Widget senderMessage(String content, String picture) {
+    scrollToBottom();
     return ListTile(
       contentPadding: const EdgeInsets.only(left: 10, right: 118),
       title: Container(
@@ -55,12 +77,8 @@ class ChatState extends State<Chat> {
           ),
           child: Text(
             content,
-            style: const TextStyle(
-              color: Colors.black,
-              fontSize: 12
-            ),
-          )
-        ),
+            style: const TextStyle(color: Colors.black, fontSize: 12),
+          )),
       leading: CircleAvatar(
         radius: 14,
         foregroundImage: NetworkImage(
@@ -72,66 +90,64 @@ class ChatState extends State<Chat> {
   }
 
   Widget reveiverMessage(String content) {
+    scrollToBottom();
     return ListTile(
       contentPadding: const EdgeInsets.only(left: 118, right: 10),
       title: Container(
-        padding: const EdgeInsets.all(10),
-        decoration: BoxDecoration(
-          color: Colors.black,
-          borderRadius: BorderRadius.circular(10.0),
-        ),
-        child: Text(
-          content,
-          style: const TextStyle(
-            color: Colors.white,
-            fontSize: 12
+          padding: const EdgeInsets.all(10),
+          decoration: BoxDecoration(
+            color: Colors.black,
+            borderRadius: BorderRadius.circular(10.0),
           ),
-        )
-      ),
+          child: Text(
+            content,
+            style: const TextStyle(color: Colors.white, fontSize: 12),
+          )),
     );
   }
 
   Widget conversation() {
     return StreamBuilder(
-        stream: FirebaseFirestore.instance
-            .collection('message')
-            .doc("0")
-            .snapshots(),
-        builder: (BuildContext context, AsyncSnapshot<DocumentSnapshot> snapshot) {
-          if (snapshot.hasError) {
-            return Center(child: Text('Error: ${snapshot.error}'));
-          }
+      stream:
+          FirebaseFirestore.instance.collection('message').doc("0").snapshots(),
+      builder:
+          (BuildContext context, AsyncSnapshot<DocumentSnapshot> snapshot) {
+        if (snapshot.hasError) {
+          return Center(child: Text('Error: ${snapshot.error}'));
+        }
 
-          if (snapshot.connectionState == ConnectionState.waiting) {
-            return Center(child: CircularProgressIndicator());
-          }
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return Center(child: CircularProgressIndicator());
+        }
 
-          // Ensure the document exists and has data
-          if (!snapshot.hasData || !snapshot.data!.exists) {
-            return Center(child: Text('Document does not exist'));
-          }
+        // Ensure the document exists and has data
+        if (!snapshot.hasData || !snapshot.data!.exists) {
+          return Center(child: Text('Document does not exist'));
+        }
 
-          // Get the messages array
-          List<dynamic> messages = snapshot.data!.get('messages') ?? [];
+        // Get the messages array
+        List<dynamic> messages = snapshot.data!.get('messages') ?? [];
 
-          if (messages.isEmpty) {
-            return const Center(child: Text('No messages yet'));
-          }
+        if (messages.isEmpty) {
+          return const Center(child: Text('No messages yet'));
+        }
 
-          return ListView.builder(
-            itemCount: messages.length,
-            itemBuilder: (context, index) {
-              final message = messages[index];
+        return ListView.builder(
+          controller: scrollController,
+          itemCount: messages.length,
+          itemBuilder: (context, index) {
+            final message = messages[index];
 
-              if (message['sender'] == user.uid) {
-                return reveiverMessage(message['message']);
-              } else {
-                return senderMessage(message['message'], secondaryUser["profile_picture"]);
-              }
-            },
-          );
-        },
-      );
+            if (message['sender'] == user.uid) {
+              return reveiverMessage(message['message']);
+            } else {
+              return senderMessage(
+                  message['message'], secondaryUser["profile_picture"]);
+            }
+          },
+        );
+      },
+    );
   }
 
   @override
@@ -141,18 +157,22 @@ class ChatState extends State<Chat> {
       appBar: AppBar(
         backgroundColor: Colors.white,
         title: Row(
-          mainAxisSize: MainAxisSize.min, // Ensures the Row takes minimal space
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            CircleAvatar(
+            mainAxisSize:
+                MainAxisSize.min, // Ensures the Row takes minimal space
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              CircleAvatar(
                 radius: 20,
-                foregroundImage:
-                    NetworkImage(
-                      secondaryUser != null ? secondaryUser["profile_picture"] : "https://www.pngitem.com/pimgs/m/146-1468479_my-profile-icon-blank-profile-picture-circle-hd.png",
-                    ),
+                foregroundImage: NetworkImage(
+                  secondaryUser != null
+                      ? secondaryUser["profile_picture"]
+                      : "https://www.pngitem.com/pimgs/m/146-1468479_my-profile-icon-blank-profile-picture-circle-hd.png",
+                ),
                 backgroundColor: const Color(0x00FFFFFF),
               ),
-              const SizedBox(width: 10,),
+              const SizedBox(
+                width: 10,
+              ),
               Text(
                 secondaryUser != null ? secondaryUser["name"] : 'Loading...',
                 style: const TextStyle(
@@ -161,47 +181,49 @@ class ChatState extends State<Chat> {
                   decoration: TextDecoration.underline,
                 ),
               ),
-           ]
-        ),
+            ]),
         centerTitle: true,
       ),
       body: Column(
         children: <Widget>[
           Expanded(
-            child: messages != null ? conversation() : const Center(child: CircularProgressIndicator()),
+            child: messages != null
+                ? conversation()
+                : const Center(child: CircularProgressIndicator()),
           ),
           Container(
-            padding: const EdgeInsets.symmetric(vertical: 20, horizontal: 25),
-            child:
-            Container(
-              height: 50,
-              padding: const EdgeInsets.only(left: 10, right: 10),
-              child: TextField(
-                controller: messageController,
-                decoration: InputDecoration(
-                  filled: true,
-                  fillColor: const Color(0xFFEEEEEE),
-                  border: OutlineInputBorder(
-                    borderSide: BorderSide.none,
-                    borderRadius: BorderRadius.circular(50),
-                  ),
-                  hintText: 'Message ...',
-                  suffixIcon: IconButton(
-                    onPressed: () {
-                      if (messageController.text.trim().isNotEmpty){
-                        if (kDebugMode) {
-                          print("Message sent: ${messageController.text.trim()}");
-                        }
-                        Database().sendMessage(conversationId, user.uid, secondaryUser["uid"], messageController.text.trim());
-                        messageController.clear();
-                      }
-                    },
-                    icon: const Icon(Icons.send),
-                  )
-                ),
-              )
-            )
-          ),
+              padding: const EdgeInsets.symmetric(vertical: 20, horizontal: 25),
+              child: Container(
+                  height: 50,
+                  padding: const EdgeInsets.only(left: 10, right: 10),
+                  child: TextField(
+                    controller: messageController,
+                    decoration: InputDecoration(
+                        filled: true,
+                        fillColor: const Color(0xFFEEEEEE),
+                        border: OutlineInputBorder(
+                          borderSide: BorderSide.none,
+                          borderRadius: BorderRadius.circular(50),
+                        ),
+                        hintText: 'Message ...',
+                        suffixIcon: IconButton(
+                          onPressed: () {
+                            if (messageController.text.trim().isNotEmpty) {
+                              if (kDebugMode) {
+                                print(
+                                    "Message sent: ${messageController.text.trim()}");
+                              }
+                              Database().sendMessage(
+                                  conversationId,
+                                  user.uid,
+                                  secondaryUser["uid"],
+                                  messageController.text.trim());
+                              messageController.clear();
+                            }
+                          },
+                          icon: const Icon(Icons.send),
+                        )),
+                  ))),
         ],
       ),
       bottomNavigationBar: const SuperNavigationBar(index: 0),
